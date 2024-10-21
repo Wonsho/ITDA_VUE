fae28b6e90afba2fd238739db100427a718b6920
wonho96 <wono96@naver.com>
2024-10-21 22:30:13 +0900

    doc

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -1,3 +1,266 @@
 <template>
+  <main class="index-main">
 
+    <section class="pop-base">
+      <h1>가장 인기있는 과정</h1>
+      <div>
+        <h2>AI 활용 빅데이터 분석 및 응용 소프트 웨어 개발자<span>그린아카데미(홍대학원)</span></h2>
+        <table>
+          <tr>
+            <th>지역</th>
+            <td>서울</td>
+          </tr>
+          <tr>
+            <th>직무분류</th>
+            <td>SW 소프트 웨어</td>
+          </tr>
+          <tr>
+            <th>훈련기간</th>
+            <td>2023-12-28 ~ 2024-02-25</td>
+          </tr>
+        </table>
+      </div>
+
+    </section>
+
+    <div class="com-banner">
+      <h1>커뮤니티</h1>
+      <div><img src="/img/itdac.png" alt="이미지"></div>
+    </div>
+
+    <section class="com-board">
+      <h1>커뮤니티 게시글</h1>
+      <ul>
+        <li>
+          <div class="info">
+            <div>
+              닉네임
+            </div>
+            <div>
+              ·
+            </div>
+            <div>
+              조회 30
+            </div>
+            <div>
+              ·
+            </div>
+            <div>
+              35 분전
+            </div>
+
+            <div class="info2">
+              <div>
+                <img src="/icon/main/thumsupcom.svg" alt="추천아이콘">
+              </div>
+              <div>
+                25
+              </div>
+
+              <div>
+                <img src="/icon/main/msg.svg"  alt="추천아이콘">
+              </div>
+              <div>
+                25
+              </div>
+            </div>
+          </div>
+          <p>Spring MVC 이클립스 TOMCAT 에서는 실행이 되나?</p>
+        </li>
+
+        <li>
+          <div class="info">
+            <div>
+              닉네임
+            </div>
+            <div>
+              ·
+            </div>
+            <div>
+              조회 30
+            </div>
+            <div>
+              ·
+            </div>
+            <div>
+              35 분전
+            </div>
+
+            <div class="info2">
+              <div>
+                <img src="/icon/main/thumsupcom.svg" alt="추천아이콘">
+              </div>
+              <div>
+                25
+              </div>
+
+              <div>
+                <img src="/icon/main/msg.svg"  alt="추천아이콘">
+              </div>
+              <div>
+                25
+              </div>
+            </div>
+          </div>
+          <p>Spring MVC 이클립스 TOMCAT 에서는 실행이 되나?</p>
+        </li>
+
+        <li>
+          <div class="info">
+            <div>
+              닉네임
+            </div>
+            <div>
+              ·
+            </div>
+            <div>
+              조회 30
+            </div>
+            <div>
+              ·
+            </div>
+            <div>
+              35 분전
+            </div>
+
+            <div class="info2">
+              <div>
+                <img src="/icon/main/thumsupcom.svg" alt="추천아이콘">
+              </div>
+              <div>
+                25
+              </div>
+
+              <div>
+                <img src="/icon/main/msg.svg"  alt="추천아이콘">
+              </div>
+              <div>
+                25
+              </div>
+            </div>
+          </div>
+          <p>Spring MVC 이클립스 TOMCAT 에서는 실행이 되나?</p>
+        </li>
+
+        <li>
+          <div class="info">
+            <div>
+              닉네임
+            </div>
+            <div>
+              ·
+            </div>
+            <div>
+              조회 30
+            </div>
+            <div>
+              ·
+            </div>
+            <div>
+              35 분전
+            </div>
+
+            <div class="info2">
+              <div>
+                <img src="/icon/main/thumsupcom.svg" alt="추천아이콘">
+              </div>
+              <div>
+                25
+              </div>
+
+              <div>
+                <img src="/icon/main/msg.svg"  alt="추천아이콘">
+              </div>
+              <div>
+                25
+              </div>
+            </div>
+          </div>
+          <p>Spring MVC 이클립스 TOMCAT 에서는 실행이 되나?</p>
+        </li>
+
+        <li>
+          <div class="info">
+            <div>
+              닉네임
+            </div>
+            <div>
+              ·
+            </div>
+            <div>
+              조회 30
+            </div>
+            <div>
+              ·
+            </div>
+            <div>
+              35 분전
+            </div>
+
+            <div class="info2">
+              <div>
+                <img src="/icon/main/thumsupcom.svg" alt="추천아이콘">
+              </div>
+              <div>
+                25
+              </div>
+
+              <div>
+                <img src="/icon/main/msg.svg"  alt="추천아이콘">
+              </div>
+              <div>
+                25
+              </div>
+            </div>
+          </div>
+          <p>Spring MVC 이클립스 TOMCAT 에서는 실행이 되나?</p>
+        </li>
+      </ul>
+
+    </section>
+
+
+
+    <section class="review-section">
+      <header>
+        <h1>현실 취업 후기</h1>
+        <h2>N살 비전공자인데 취업 가능할까요??</h2>
+        <p>이제 그런 걱정은 노노! 진자 우리 주변의 이야기들, 취업 스토리를 참고해 보세요.</p>
+      </header>
+
+      <ul>
+        <li>
+          <div class="img-box"><img src="/img/도형이취향.jpeg" alt=""></div>
+          <dl class="info">
+            <dt>이름</dt>
+            <dd class="name">이도형</dd>
+            <dt>지원 분야</dt>
+            <dd class="part">백앤드</dd>
+          </dl>
+          <dl class="info2">
+            <div class="item">
+              <dt>전&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;공</dt>
+              <dd>통신공학</dd>
+            </div>
+            <div>
+              <dt>취업나이</dt>
+              <dd>28세</dd>
+            </div>
+            <div>
+              <dt>취준기간</dt>
+              <dd>약 19 개월</dd>
+            </div>
+          </dl>
+          <p>개발이 재미는 있지만 계속 하다 보니 내 asdasdasdasdsad 맞는거 같기 하다 보니 내 asdasdasdasdsad 맞는거 같기 하다 보니 내 asdasdasdasdsad 맞는거 같기 하다 보니 내 asdasdasdasdsad 맞는거 같기 하다 보니 내 asdasdasdasdsad 맞는거 같기 하다 보니 내 asdasdasdasdsad 맞는거 같기 하다 보니 내 asdasdasdasdsad 맞는거 같기 하다 보니 내 asdasdasdasdsad 맞는거 같기 하다 보니 내 asdasdasdasdsad 맞는거 같기 하다 보니 내 asdasdasdasdsad 맞는거 같기도 아닌거 같기도 코딩 개꿀잼</p>
+        </li>
+      </ul>
+    </section>
+
+  </main>
 </template>
+
+<style scoped>
+  @import "/css/index.css";
+</style>
+<script setup lang="ts">
+</script>
