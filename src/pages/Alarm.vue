83f0e9153aac80bd71ac556295102adabb3383f0
dohyeong <leelee0330@naver.com>
2024-10-22 18:07:02 +0900

    docs

--- src/pages/Alarm.vue
+++ src/pages/Alarm.vue
@@ -3,9 +3,112 @@
 </script>
 
 <template>
+  <main>
+    <!-- 제목 -->
+    <section class="alarm-header">
+      <h1 style="display: none;">알림</h1>
+      <div class="header_title">
+        알림
+      </div>
+    </section>
 
+    <!-- 댓글리스트 -->
+    <section class="alarm-comment_list">
+      <h1 style="display: none;">알림 리스트</h1>
+      <section class="comment_item">
+        <div class="item-content">
+          <span class="notification badge" ></span>
+          <div class="item-text">도형 님이 회원님의 댓글에 댓글을 남겼습니다.</div>
+          <div class="item-time">2024-01-12</div>
+        </div>
+      </section>
+      <section class="comment_item">
+        <div class="item-content">
+          <span class="notification badge" ></span>
+          <div class="item-text">도형 님이 회원님의 댓글에 댓글을 남겼습니다.</div>
+          <div class="item-time">2024-01-12</div>
+        </div>
+      </section>
+      <section class="comment_item">
+        <div class="item-content">
+          <span class="notification badge" ></span>
+          <div class="item-text">도형 님이 회원님의 댓글에 댓글을 남겼습니다.</div>
+          <div class="item-time">2024-01-12</div>
+        </div>
+      </section>
+      <section class="comment_item">
+        <div class="item-content">
+          <span class="notification badge" ></span>
+          <div class="item-text">도형 님이 회원님의 댓글에 댓글을 남겼습니다.</div>
+          <div class="item-time">2024-01-12</div>
+        </div>
+      </section>
+      <section class="comment_item">
+        <div class="item-content">
+          <span class="notification badge" ></span>
+          <div class="item-text">도형 님이 회원님의 댓글에 댓글을 남겼습니다.</div>
+          <div class="item-time">2024-01-12</div>
+        </div>
+      </section>
+      <section class="comment_item">
+        <div class="item-content">
+          <span class="notification badge" ></span>
+          <div class="item-text">도형 님이 회원님의 댓글에 댓글을 남겼습니다.</div>
+          <div class="item-time">2024-01-12</div>
+        </div>
+      </section>
+      <section class="comment_item">
+        <div class="item-content">
+          <span class="notification badge" ></span>
+          <div class="item-text">도형 님이 회원님의 댓글에 댓글을 남겼습니다.</div>
+          <div class="item-time">2024-01-12</div>
+        </div>
+      </section>
+      <section class="comment_item">
+        <div class="item-content">
+          <span class="notification badge" ></span>
+          <div class="item-text">도형 님이 회원님의 댓글에 댓글을 남겼습니다.</div>
+          <div class="item-time">2024-01-12</div>
+        </div>
+      </section>
+      <section class="comment_item">
+        <div class="item-content">
+          <span class="notification badge" ></span>
+          <div class="item-text">도형 님이 회원님의 댓글에 댓글을 남겼습니다.</div>
+          <div class="item-time">2024-01-12</div>
+        </div>
+      </section>
+      <section class="comment_item">
+        <div class="item-content">
+          <span class="notification badge" ></span>
+          <div class="item-text">도형 님이 회원님의 댓글에 댓글을 남겼습니다.</div>
+          <div class="item-time">2024-01-12</div>
+        </div>
+      </section>
+      <section class="comment_item">
+        <div class="item-content">
+          <span class="notification badge" ></span>
+          <div class="item-text">도형 님이 회원님의 댓글에 댓글을 남겼습니다.</div>
+          <div class="item-time">2024-01-12</div>
+        </div>
+      </section>
+    </section>
+
+    <section>
+      <h1 style="display: none;">커뮤니티 네비게이션</h1>
+      <div class="community_nav">
+        <a href="#" class="pgL"></a>
+        <a href="#" class="on">1</a>
+        <a href="#">2</a>
+        <a href="#">3</a>
+        <a href="#">4</a>
+        <a href="#">5</a>
+        <a href="#" class="pgR"></a>
+      </div>
+    </section>
+  </main>
 </template>
 
 <style scoped>
-
+@import url("/css/alarm.css");
 </style>
--- src/pages/mpage/Cname.vue
+++ src/pages/mpage/Cname.vue
@@ -3,9 +3,41 @@
 </script>
 
 <template>
-cname
+  <main>
+    <h1 style="display: none;">닉네임변경</h1>
+
+    <section class="mypage-profile">
+      <h1 style="display: none;">프로필</h1>
+      <div class="myprofile-content">
+        <div class="content-img_box">
+          <img src="../icon/mypage/account.svg" alt="아이콘">
+        </div>
+        <div class="content-text_box">
+          <div class="name_text">
+            재영바보
+          </div>
+          <div class="user_attribute">
+            네이버 회원
+          </div>
+        </div>
+      </div>
+    </section>
+
+    <section class="mypage-update_form">
+      <h1 style="display: none;">닉네임 변경</h1>
+
+      <div class="update_box">
+        <form action="#" class="update_form">
+          <div class="update_title">닉네임 변경</div>
+          <div class="update_text"><input type="text" placeholder="재영바보"></div>
+          <div class="update_btn"><button type="submit">변경</button></div>
+        </form>
+      </div>
+
+    </section>
+  </main>
 </template>
 
 <style scoped>
-
+@import url("/css/mypage-update.css");
 </style>
