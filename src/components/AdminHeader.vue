161489eb77335591d094713c5f860da62a06d0ba
wonho96 <wono96@naver.com>
2024-10-23 20:17:16 +0900

    doc

--- src/components/AdminHeader.vue
+++ src/components/AdminHeader.vue
@@ -3,9 +3,15 @@
 </script>
 
 <template>
-asdsa
+  <header class="admin-header">
+    <h1>ITDA</h1>
+    <div>
+      <img @click="" src="/icon/admin_header/menu.svg" alt="메뉴">
+    </div>
+    <div>관리자명</div>
+  </header>
 </template>
 
 <style scoped>
-
+  @import "/css/admin-header.css";
 </style>
--- src/pages/admin/Index.vue
+++ src/pages/admin/Index.vue
@@ -3,7 +3,6 @@
 </script>
 
 <template>
-
   <main>
 
   </main>
