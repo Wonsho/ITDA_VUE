0d4e1b567d4c2ef4cd999ae1bc7bb11bdb7535b1
wonho96 <wono96@naver.com>
2024-10-23 17:53:03 +0900

    modify

--- src/components/BottomBar.vue
+++ src/components/BottomBar.vue
@@ -6,11 +6,11 @@
   <nav class="bottom">
     <h1 style="display: none">하단 메뉴</h1>
     <ul class="bottom-bar">
-      <li><a href=""><img src="/icon/bottom-bar/community.svg" alt="">커뮤니티</a></li>
-      <li><a href=""><img src="/icon/bottom-bar/academy.svg" alt="">국비학원</a></li>
-      <li class="home"><a href=""><img src="/icon/bottom-bar/home.svg" alt="">홈</a></li>
-      <li><a href=""><img src="/icon/bottom-bar/mypick.svg" alt="">마이픽</a></li>
-      <li><a href=""><img src="/icon/bottom-bar/profile.svg" alt="">프로필</a></li>
+      <li><a href="/community"><img src="/icon/bottom-bar/community.svg" alt="">커뮤니티</a></li>
+      <li><a href="/academies"><img src="/icon/bottom-bar/academy.svg" alt="">국비학원</a></li>
+      <li class="home"><a href="/index"><img src="/icon/bottom-bar/home.svg" alt="">홈</a></li>
+      <li><a href="/mpage/mypic"><img src="/icon/bottom-bar/mypick.svg" alt="">마이픽</a></li>
+      <li><a href="/mpage"><img src="/icon/bottom-bar/profile.svg" alt="">프로필</a></li>
     </ul>
   </nav>
 </template>
