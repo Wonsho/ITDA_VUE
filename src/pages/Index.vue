3909ccb978d5dcc4ce864f72f873002afdad9153
wonho96 <wono96@naver.com>
2024-10-27 20:10:10 +0900

    doc

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -23,6 +23,47 @@
 
     </section>
 
+
+
+
+    <section class="review-section">
+      <header>
+        <h1><span>N살 비전공자</span>인데..</h1>
+        <h2>취업할 수 있을까요?</h2>
+      </header>
+
+      <ul>
+        <li>
+          <div class="img-box"><img src="/img/도형이취향.jpeg" alt=""></div>
+          <dl class="info">
+            <dt>이름</dt>
+            <dd class="name">이도형</dd>
+            <dt>지원 분야</dt>
+            <dd class="part">백앤드</dd>
+          </dl>
+          <dl class="info2">
+            <div class="item">
+              <dt>전&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;공</dt>
+              <dd>통신공학</dd>
+            </div>
+            <div>
+              <dt>취업나이</dt>
+              <dd>28세</dd>
+            </div>
+            <div>
+              <dt>취준기간</dt>
+              <dd>약 19 개월</dd>
+            </div>
+          </dl>
+          <p>개발이 재미는 있지만 계속 하다 보니 내 asdasdasdasdsad 맞는거 같기 하다 보니 내 asdasdasdasdsad 맞는거 같기 하다 보니 내 asdasdasdasdsad 맞는거 같기 하다 보니 내 asdasdasdasdsad 맞는거 같기 하다 보니 내 asdasdasdasdsad 맞는거 같기 하다 보니 내 asdasdasdasdsad 맞는거 같기 하다 보니 내 asdasdasdasdsad 맞는거 같기 하다 보니 내 asdasdasdasdsad 맞는거 같기 하다 보니 내 asdasdasdasdsad 맞는거 같기 하다 보니 내 asdasdasdasdsad 맞는거 같기도 아닌거 같기도 코딩 개꿀잼</p>
+        </li>
+      </ul>
+    </section>
+
+    <div class="review-btn">
+      <a href="">취업 후기 확인하기</a>
+    </div>
+
     <div class="com-banner">
       <h1>커뮤니티</h1>
       <div><img src="/img/itdac.png" alt="이미지"></div>
@@ -217,43 +258,6 @@
         </li>
       </ul>
 
-    </section>
-
-
-
-    <section class="review-section">
-      <header>
-        <h1>현실 취업 후기</h1>
-        <h2>N살 비전공자인데 취업 가능할까요??</h2>
-        <p>이제 그런 걱정은 노노! 진자 우리 주변의 이야기들, 취업 스토리를 참고해 보세요.</p>
-      </header>
-
-      <ul>
-        <li>
-          <div class="img-box"><img src="/img/도형이취향.jpeg" alt=""></div>
-          <dl class="info">
-            <dt>이름</dt>
-            <dd class="name">이도형</dd>
-            <dt>지원 분야</dt>
-            <dd class="part">백앤드</dd>
-          </dl>
-          <dl class="info2">
-            <div class="item">
-              <dt>전&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;공</dt>
-              <dd>통신공학</dd>
-            </div>
-            <div>
-              <dt>취업나이</dt>
-              <dd>28세</dd>
-            </div>
-            <div>
-              <dt>취준기간</dt>
-              <dd>약 19 개월</dd>
-            </div>
-          </dl>
-          <p>개발이 재미는 있지만 계속 하다 보니 내 asdasdasdasdsad 맞는거 같기 하다 보니 내 asdasdasdasdsad 맞는거 같기 하다 보니 내 asdasdasdasdsad 맞는거 같기 하다 보니 내 asdasdasdasdsad 맞는거 같기 하다 보니 내 asdasdasdasdsad 맞는거 같기 하다 보니 내 asdasdasdasdsad 맞는거 같기 하다 보니 내 asdasdasdasdsad 맞는거 같기 하다 보니 내 asdasdasdasdsad 맞는거 같기 하다 보니 내 asdasdasdasdsad 맞는거 같기 하다 보니 내 asdasdasdasdsad 맞는거 같기도 아닌거 같기도 코딩 개꿀잼</p>
-        </li>
-      </ul>
     </section>
 
   </main>
